5c3067852dd500402d0ea5654dd46d1ef74322c0
Maksim <Maksim>
2024-11-20 18:51:53 +0300

    remove explicit compare

--- jacobi/jacobi.cu
+++ jacobi/jacobi.cu
@@ -35,9 +35,9 @@ __global__ void function(const double *A, double *B) {
     int j = blockIdx.y * blockDim.y + threadIdx.y;
     int i = blockIdx.z * blockDim.z + threadIdx.z;
 
-    if ((i > 0) && (i < L - 1)) {
-        if ((j > 0) && (j < L - 1)) {
-            if ((k > 0) && (k < L - 1)) {
+    if (i < L - 1) {
+        if (j < L - 1) {
+            if (k < L - 1) {
                 B(i, j, k) = (A(i - 1, j, k) + A(i, j - 1, k) + A(i, j, k - 1) + A(i, j, k + 1) + A(i, j + 1, k) + A(i + 1, j, k)) / 6.0;
             }
         }
@@ -49,9 +49,9 @@ __global__ void difference_ab(double *A, const double *B, double* eps) {
     int j = blockIdx.y * blockDim.y + threadIdx.y;
     int i = blockIdx.z * blockDim.z + threadIdx.z;
 
-    if ((i > 0) && (i < L - 1)) {
-        if ((j > 0) && (j < L - 1)) {
-            if ((k > 0) && (k < L - 1)) {
+    if (i < L - 1) {
+        if (j < L - 1) {
+            if (k < L - 1) {
                 eps(i, j, k) = fabs(B(i, j, k) - A(i, j, k));
                 A(i, j, k) = B(i, j, k);
             }
