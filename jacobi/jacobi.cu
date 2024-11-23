e0fb37ba2e4e2197c28bc11acc47b27ac6c6902a
Maksim <Maksim>
2024-11-23 22:25:06 +0300

    debug

--- jacobi/jacobi.cu
+++ jacobi/jacobi.cu
@@ -174,7 +174,7 @@ int main(int an, char **as)
             
             SAFE_CALL(cudaMemcpy(A_device, B_device, size, cudaMemcpyDeviceToDevice));
 
-            std::cout << '!';
+            std::cout << eps;
             if (eps < MAXEPS)
                 break;
         }
