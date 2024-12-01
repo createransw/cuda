8c3e4bb22d1aaacfb4c29316507ecf965a7112bd
Maksim <Maksim>
2024-12-02 01:56:32 +0300

    try 100

--- ADI/adi.cu
+++ ADI/adi.cu
@@ -22,9 +22,9 @@
 #define B(i, j, k) B[((i) * nx + (j)) * ny + (k)]
 #define eps(i, j, k) eps[((i) * nx + (j)) * ny + (k)]
 
-#define nx 31
-#define ny 31
-#define nz 31
+#define nx 100
+#define ny 100
+#define nz 100
         
 
 double maxeps = 0.01;
