52dd53a0d7d3e154cd7de84acff16825ee5fe49d
Maksim <Maksim>
2024-12-02 02:20:35 +0300

    fix

--- ADI/adi.cu
+++ ADI/adi.cu
@@ -46,6 +46,7 @@ __global__ void function(double *A, double *eps, char dim) {
 
     if (dim == 'i') {
         if ((threadIdx.x == 0) && (threadIdx.y == 0) && (threadIdx.z == 0)) {
+            do {}
             while (atomicAdd(&dim_count, 0) < i * gridDim.x * gridDim.y);
         }
         __syncthreads();
@@ -57,6 +58,7 @@ __global__ void function(double *A, double *eps, char dim) {
 
     if (dim == 'j') {
         if ((threadIdx.x == 0) && (threadIdx.y == 0) && (threadIdx.z == 0)) {
+            do {}
             while (atomicAdd(&dim_count, 0) < j * gridDim.x * gridDim.z);
         }
         __syncthreads();
@@ -68,6 +70,7 @@ __global__ void function(double *A, double *eps, char dim) {
 
     if (dim == 'k') {
         if ((threadIdx.x == 0) && (threadIdx.y == 0) && (threadIdx.z == 0)) {
+            do {}
             while (atomicAdd(&dim_count, 0) < k * gridDim.y * gridDim.z);
         }
         __syncthreads();
