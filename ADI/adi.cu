d4443cd522365ee9fb04d7f082b5cd727c894103
Maksim <Maksim>
2024-12-02 18:42:32 +0300

    debug

--- ADI/adi.cu
+++ ADI/adi.cu
@@ -180,6 +180,7 @@ int main(int argc, char *argv[])
 
         SAFE_CALL(cudaEventRecord(startt, 0));
         for (int it = 1; it <= itmax; it++) {
+            std::cerr << "!";
             set<<<1, 1>>>();
             function<<<gridDim_i, blockDim_i>>>(A_device, ptrdiff, 'i');
             set<<<1, 1>>>();
