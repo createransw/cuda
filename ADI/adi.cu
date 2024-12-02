5bb8a15ca1d794272454668c405fdfe91bd8a49e
Maksim <Maksim>
2024-12-02 18:56:13 +0300

    debug

--- ADI/adi.cu
+++ ADI/adi.cu
@@ -181,10 +181,10 @@ int main(int argc, char *argv[])
 
         SAFE_CALL(cudaEventRecord(startt, 0));
         for (int it = 1; it <= itmax; it++) {
-            eps = thrust::reduce(diff.begin(), diff.end(), 0.0, thrust::maximum<double>());
             std::cerr << "!";
             set<<<1, 1>>>();
             function<<<gridDim_i, blockDim_i>>>(A_device, ptrdiff, 'i');
+            eps = thrust::reduce(diff.begin(), diff.end(), 0.0, thrust::maximum<double>());
             std::cerr << "!";
             set<<<1, 1>>>();
             function<<<gridDim_j, blockDim_j>>>(A_device, ptrdiff, 'j');
