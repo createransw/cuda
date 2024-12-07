fa91532dceca0cff026050ae77d40663e848846f
Maksim <Maksim>
2024-12-08 00:20:43 +0300

    debug

--- ADI/adi.cu
+++ ADI/adi.cu
@@ -25,9 +25,9 @@
 #define eps(i, j, k) eps[((i) * ny + (j)) * nz + (k)]
 #define temp(i, j, k) temp[((i) * 8 + (j)) * 8 + (k)]
 
-#define nx 7
-#define ny 7
-#define nz 7
+#define nx 50
+#define ny 50
+#define nz 50
         
 
 double maxeps = 0.01;
