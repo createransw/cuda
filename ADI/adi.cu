460c04ee8a2030342695456fc439ac0e63fd2f30
Maksim <Maksim>
2024-12-23 19:09:12 +0300

    size

--- ADI/adi.cu
+++ ADI/adi.cu
@@ -27,9 +27,9 @@
 #define temp_j(i, j, k) temp_j[((i) * 64 + (j)) * 4 + (k)]
 #define temp_k(i, j, k) temp_k[((i) * 4 + (j)) * 64 + (k)]
 
-#define nx 800
-#define ny 800
-#define nz 800
+#define nx 600
+#define ny 600
+#define nz 600
         
 
 double maxeps = 0.01;
